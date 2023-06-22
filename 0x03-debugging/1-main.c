bb76f1b1904be17f493b81e06c695a9a50b6f879
Stanley0022 <ayenistanley21@gmail.com>
2023-06-22 15:07:34 -0700

    Debugging Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - Avoids an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
-        int i;
+    int i;
 
-        printf("Infinite loop incoming :(\n");
+    printf("Infinite loop incoming :(\n");
 
-        i = 0;
+    i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+    while (i < 10)
+    {
+        putchar('0' + i);  // Print the character representation of the digits 0-9
+        i++;              // Increment i to avoid the infinite loop
+    }
 
-        printf("Infinite loop avoided! \\o/\n");
+    printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+    return (0);
 }
 
